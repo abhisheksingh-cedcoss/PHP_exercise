97f28175d16b024992686390d8fd50f40244dcee
abhisheksingh-cedcoss <abhisheksingh2022jan@cedcommerce.com>
2022-02-21 15:00:43 +0530

    getting the first name in output from form

--- src/index.php
+++ src/index.php
@@ -202,6 +202,16 @@ asort($age);
 
 
 
-
+<form action="welcome.php" method="get">
+First name: <input type="text" name="fname">
+</form>
 
 ?>
+
+<html>
+<body>
+
+Welcome <?php echo $_GET["fname"]; ?>
+
+</body>
+</html>
